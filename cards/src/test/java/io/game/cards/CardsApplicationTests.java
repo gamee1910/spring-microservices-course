9e74db95b1ae7206d28dcd14478168a04d06faa0
game <hhhau1910@gmail.com>
2026-05-05 15:30:59 +0700

    feat: initialize cards and loans applications with Maven setup and basic configurations

---
+++ cards/src/test/java/io/game/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.game.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
